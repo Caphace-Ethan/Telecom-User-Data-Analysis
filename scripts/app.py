64fb1a2327d5c5bb08243e616e18f193da1504c6
Ethan <ethani.kitilah@eganet.go.tz>
2021-07-18 22:44:57 +0300

    Modified Streamlit Dashboard Script

--- scripts/app.py
+++ scripts/app.py
@@ -3,26 +3,25 @@ import pandas as pd
 import streamlit as st
 import altair as alt
 import plotly.express as px
-from add_data import db_execute_fetch
 
 st.set_page_config(page_title="Dashboard | Telecom User Data Analysis ", layout="wide")
 
 def loadData():
-    query = "select * from TelecomUserData"
-    df = db_execute_fetch(query, dbName="TelecomUserData", rdf=True)
-    return df
+    pd.set_option('max_column', None)
+    loaded_data = pd.read_csv('./data/processed_data.csv')
+    return loaded_data
 
 def selectHandset():
     df = loadData()
-    handset = st.multiselect("choose Handset Type(s)", list(df['handset_type'].unique()))
+    handset = st.multiselect("choose Handset Type(s)", list(df['Handset Type'].unique()))
     if handset:
         df = df[np.isin(df, handset).any(axis=1)]
         st.write(df)
 
 def selectHandsetManufac():
     df = loadData()
-    manufacturer = st.multiselect("choose Handset Manufacturer", list(df['handset_manufacturer'].unique()))
-    duration = st.multiselect("choose Time spent on session", list(df['duration_ms'].unique()))
+    manufacturer = st.multiselect("choose Handset Manufacturer", list(df['Handset Manufacturer'].unique()))
+    duration = st.multiselect("choose Time spent on session", list(df['Dur. (ms)'].unique()))
 
     if manufacturer and not duration:
         df = df[np.isin(df, manufacturer).any(axis=1)]
@@ -47,43 +46,44 @@ def barChart(data, title, X, Y):
 
 def stBarChart():
     df = loadData()
-    dfCount = pd.DataFrame({'Session_count': df.groupby(['msisdn_number'])['bearer_id'].count()}).reset_index()
-    dfCount["msisdn_number"] = dfCount["msisdn_number"].astype(str)
+    dfCount = pd.DataFrame({'Session_count': df.groupby(['MSISDN/Number'])['Bearer Id'].count()}).reset_index()
+    dfCount["MSISDN/Number"] = dfCount["MSISDN/Number"].astype(str)
     dfCount = dfCount.sort_values("Session_count", ascending=False)
 
     num = st.slider("Select number of Rankings", 0, 50, 5)
     title = f"Top {num} Sessions Ranking By User"
-    barChart(dfCount.head(num), title, "msisdn_number", "Session_count")
+    barChart(dfCount.head(num), title, "MSISDN/Number", "Session_count")
 
 def DataUsageBarChart():
     df = loadData()
-    dfCount = pd.DataFrame({'Session_count': df.groupby(['handset_type'])['bearer_id'].count()}).reset_index()
-    dfCount["handset_type"] = dfCount["handset_type"].astype(str)
+    dfCount = pd.DataFrame({'Session_count': df.groupby(['Handset Type'])['Bearer Id'].count()}).reset_index()
+    dfCount["Handset Type"] = dfCount["Handset Type"].astype(str)
     dfCount = dfCount.sort_values("Session_count", ascending=False)
 
     # num = st.slider("Select number of Rankings", 0, 50, 5)
     num = 5
     title = f"Top {num} Sessions Ranking By Type of Handset"
-    barChart(dfCount.head(num), title, "handset_type", "Session_count")
+    barChart(dfCount.head(num), title, "Handset Type", "Session_count")
 
 def DataUsagePerAppChart():
     df = loadData()
-    dfLangCount = pd.DataFrame({'Data_Usage': df.groupby(['handset_type'])['social_media_data', 'google_data',
-                                                                           'email_data', 'youtube_data', 'netflix_data',
-                                                                           'gaming_data', 'total_dl_ul'].sum()}).reset_index()
-    dfLangCount["handset_type"] = dfLangCount["handset_type"].astype(str)
-    dfLangCount = dfLangCount.sort_values("Data_Usage", ascending=False)
-    dfLangCount.loc[dfLangCount['Data_Usage'] < 10, 'handset_type'] = 'Other Handsets'
-    st.title(" User data per Handsets pie chart")
-    fig = px.pie(dfLangCount, values='Data_Usage', names='handset_type', width=500, height=350)
-    fig.update_traces(textposition='inside', textinfo='percent+label')
+    app_data_list = ['social_media_data', 'google_data','email_data', 'youtube_data', 
+                     'netflix_data', 'gaming_data', 'total_dl_ul']
+    for column in app_data_list:                 
+        dfLangCount = pd.DataFrame({'Data_Usage': df.groupby(['Handset Type'])[column].sum()}).reset_index()
+        dfLangCount["Handset Type"] = dfLangCount["Handset Type"].astype(str)
+        dfLangCount = dfLangCount.sort_values("Data_Usage", ascending=False)
+        dfLangCount.loc[dfLangCount['Data_Usage'] < 10, 'Handset Type'] = 'Other Handsets'
+        st.title(" User data per Handsets pie chart")
+        fig = px.pie(dfLangCount, values='Data_Usage', names='Handset Type', width=500, height=350)
+        fig.update_traces(textposition='inside', textinfo='percent+label')
 
-    colB1, colB2 = st.beta_columns([2.5, 1])
+        colB1, colB2 = st.beta_columns([2.5, 1])
 
-    with colB1:
-        st.plotly_chart(fig)
-    with colB2:
-        st.write(dfLangCount)
+        with colB1:
+            st.plotly_chart(fig)
+        with colB2:
+            st.write(dfLangCount)
 
 st.markdown("<h1 style='color:#0b4eab;font-size:36px;border-radius:10px;'>Dashboard | Telecommunication Users Data Analysis </h1>", unsafe_allow_html=True)
 selectHandset()
